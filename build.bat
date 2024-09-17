e546cf89387e82f7d6cb91311d85dbdd7789aa28
Sander <sqndxr@gmail.com>
2024-09-17 09:21:16 +0200

    script fix and gitignore fix

--- build.bat
+++ build.bat
@@ -4,20 +4,32 @@ REM Exit immediately if a command exits with a non-zero status
 SETLOCAL ENABLEEXTENSIONS
 SET ERRORLEVEL=0
 
+echo Starting build script...
+
 REM Navigate to the frontend directory
 cd frontend || goto :error
+echo In frontend directory: %CD%
 
 REM Run the build command using bun
 bun run build || goto :error
 
-REM Navigate to the backend\public directory
-cd ..\backend\public || goto :error
+REM Navigate to the backend directory
+cd ..\backend || goto :error
+echo In backend directory: %CD%
+
+REM Build the Go backend
+go build -o mango.exe || goto :error
+
+REM Navigate to the public directory
+cd public || goto :error
+echo In public directory: %CD%
 
 REM Delete index.html
 del /F /Q index.html
 
 REM Go back to the root directory
-cd ..\..\..
+cd ..\..
+echo Back to root directory: %CD%
 
 REM Move mango.exe from backend to the root directory
 move /Y backend\mango.exe . || goto :error
@@ -26,5 +38,6 @@ echo Script executed successfully.
 goto :eof
 
 :error
-echo An error occurred. Exiting script.
+echo An error occurred at %CD%. Exiting script.
+pause
 exit /b 1
--- build.sh
+++ build.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status.
+# Exit immediately if a command exits with a non-zero status
 set -e
+
+echo "Starting build script..."
 
 # Navigate to the frontend directory
 cd frontend || exit 1
+echo "In frontend directory: $(pwd)"
 
 # Run the build command using bun
 bun run build
 
-# Navigate to the backend/public directory
-cd ../backend/public || exit 1
+# Navigate to the backend directory
+cd ../backend || exit 1
+echo "In backend directory: $(pwd)"
+
+# Build the Go backend
+go build -o mango
+
+# Navigate to the public directory
+cd public || exit 1
+echo "In public directory: $(pwd)"
 
 # Delete index.html
 rm -f index.html
 
 # Go back to the root directory
-cd ../../..
+cd ../..
+echo "Back to root directory: $(pwd)"
 
-# Move mango.exe from backend to the root directory
-mv backend/mango.exe .
+# Move mango executable from backend to the root directory
+mv backend/mango .
 
 echo "Script executed successfully."
